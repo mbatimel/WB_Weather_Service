670fac8d796e1852f1a02c444c2f2a548d412e74
mangushev-ildar <mangushev.ildar@bk.ru>
2024-07-06 18:45:19 +0300

    make additions

--- internal/repo/repoWeather.go
+++ internal/repo/repoWeather.go
@@ -15,7 +15,7 @@ import (
 const (
 	geocodingAPIURL   = "http://api.openweathermap.org/geo/1.0/direct"
 	openWeatherAPIURL = "https://api.openweathermap.org/data/2.5/forecast"
-	openWeatherAPIKey = "ea97a3b324b49ab2208278142513501d"
+	openWeatherAPIKey = "api"
 )
 
 func (db *DataBase) InitializeCities() error {
