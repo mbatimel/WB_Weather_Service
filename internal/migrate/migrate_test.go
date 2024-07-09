545b251d74d5b90d8d8f41a90d0028203ac916fd
mangushev-ildar <mangushev.ildar@bk.ru>
2024-07-09 14:30:09 +0300

    make test

---
+++ internal/migrate/migrate_test.go
@@ -0,0 +1,25 @@
+package migrate
+
+import ("testing"
+"github.com/mbatimel/WB_Weather_Service/internal/repo"
+)
+
+func TestApplyMigrations(t *testing.T) {
+	db, err := repo.SetConfigs("config/config.yaml")
+	want :=error(nil)
+    if err != nil {
+		t.Errorf("got %q, wanted %q", err, want)
+    }
+    defer db.Close()
+
+    err = db.ConnectToDataBase()
+    if err != nil {
+		t.Errorf("got %q, wanted %q", err, want)
+    }
+
+    // Example migration application
+    err = ApplyMigrations(db, "migrations/migrate.sql")
+    if err != nil {
+		t.Errorf("got %q, wanted %q", err, want)
+    }
+}
--- internal/repo/repo_test.go
+++ internal/repo/repo_test.go
@@ -1,14 +1,117 @@
 package repo
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestConnect(t *testing.T){}
-func TestGetAllCities(t *testing.T) {}
+func TestInitializeCities(t *testing.T){
+	db, err := SetConfigs("config/config.yaml")
+    want := error(nil)
+	if want != err{
+		t.Errorf("got %q, wanted %q", err, want)
+	}
+    defer db.Close()
 
-func TestGetShortInfoCity(t *testing.T) {}
+    err = db.ConnectToDataBase()
+    if err != want {
+        t.Errorf("got %q, wanted %q", err, want)
+    }
+	//посмотреть что в БД закинуто
 
-func TestGetFullInfoCity(t *testing.T) {}
+}
 
-func TestInitializeCities(t *testing.T){}
+func TestUpdateWeatherForecast(t *testing.T){
+	db, err := SetConfigs("config/config.yaml")
+    want := error(nil)
+	if want != err{
+		t.Errorf("got %q, wanted %q", err, want)
+	}
+    defer db.Close()
 
-func TestUpdateWeatherForecast(t *testing.T){}
+    err = db.ConnectToDataBase()
+    if err != want {
+        t.Errorf("got %q, wanted %q", err, want)
+    }
+	//посмотреть что в БД закинуто
+}
+func TestConnect(t *testing.T){
+	db, err := SetConfigs("config/config.yaml")
+    want := error(nil)
+	if want != err{
+		t.Errorf("got %q, wanted %q", err, want)
+	}
+    defer db.Close()
+
+    err = db.ConnectToDataBase()
+    if err != want {
+        t.Errorf("got %q, wanted %q", err, want)
+    }
+}
+func TestGetAllCities(t *testing.T) {
+	db, err := SetConfigs("config/config.yaml")
+    want := error(nil)
+	if want != err{
+		t.Errorf("got %q, wanted %q", err, want)
+	}
+    defer db.Close()
+
+    err = db.ConnectToDataBase()
+    if err != want {
+        t.Errorf("got %q, wanted %q", err, want)
+    }
+	wantRes:=[]string{"London", "Paris", "Berlin", "New York", "Tokyo"}
+	result, errDB := db.GetAllCities()
+	if errDB != want {
+		t.Errorf("got %q, wanted %q", errDB, want)
+	}
+	if len(wantRes) !=len(result){
+		t.Errorf("got %q, wanted %q", result, wantRes)
+	}
+}
+
+func TestGetShortInfoCity(t *testing.T) {
+	db, err := SetConfigs("config/config.yaml")
+    want := error(nil)
+	if want != err{
+		t.Errorf("got %q, wanted %q", err, want)
+	}
+    defer db.Close()
+
+    err = db.ConnectToDataBase()
+    if err != want {
+        t.Errorf("got %q, wanted %q", err, want)
+    }
+	// ЗАМЕНИТЬ
+	wantRes:=[]string{"London", "Paris", "Berlin", "New York", "Tokyo"}
+	result, errDB := db.GetShortInfoCity("London")
+	if errDB != want {
+		t.Errorf("got %q, wanted %q", errDB, want)
+	}
+	if len(wantRes) !=len(result){
+		t.Errorf("got %q, wanted %q", result, wantRes)
+	}
+}
+
+func TestGetFullInfoCity(t *testing.T) {
+	db, err := SetConfigs("config/config.yaml")
+    want := error(nil)
+	if want != err{
+		t.Errorf("got %q, wanted %q", err, want)
+	}
+    defer db.Close()
+
+    err = db.ConnectToDataBase()
+    if err != want {
+        t.Errorf("got %q, wanted %q", err, want)
+    }
+	// ЗАМЕНИТЬ
+	// wantRes:=[]string{"London", "Paris", "Berlin", "New York", "Tokyo"}
+	// result, errDB := db.GetFullInfoCity("London", 2024-06-08)
+	// if errDB != want {
+	// 	t.Errorf("got %q, wanted %q", errDB, want)
+	// }
+	// if len(wantRes) !=len(result){
+	// 	t.Errorf("got %q, wanted %q", result, wantRes)
+	// }
+}
+
